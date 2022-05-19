599f9d04167e7fb49dec0636163d114ee6b71524
KayckSchuenck <ycferreiras@gmail.com>
2022-05-19 00:05:57 -0300

    versao inicial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { func } from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
+import './public/reset.css'
+import './style.css'
+
+function App(){
+    
+}
+
+ReactDOM.render(
+    <App />, document.querySelector('.root')
+  );
+  
